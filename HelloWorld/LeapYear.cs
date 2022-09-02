3348b889a5ce8aa39e0bb376a67cacf1fb0bce09
Rasmus Nielsen <rasni@itu.dk>
2022-09-02 14:58:59 +0200

    LeapYear test (#2)

---
+++ HelloWorld/LeapYear.cs
@@ -0,0 +1,34 @@
+﻿namespace HelloWorld;
+
+public class LeapYear
+{
+    public bool IsLeapYear(int year)
+    {
+        var output = false;
+
+        if (year % 4 == 0)
+        {
+            if (year % 100 == 0)
+            {
+                if (year % 400 == 0)
+                {
+                    output = true;
+                }
+                else
+                {
+                    output = false;
+                }
+            }
+            else
+            {
+                output = true;
+            }
+        }
+        else
+        {
+            output = false;
+        }
+        return output;
+
+    }
+}
